b22ecb867dada33f34fe939ef02e581b6c2b3d99
RobertoAlessandroGithub <robertoalessandro_2226250030@mhs.mdp.ac.id>
2023-12-21 15:02:57 +0700

    token ability

--- routes/api.php
+++ routes/api.php
@@ -22,22 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('fakultas', [FakultasController::class, 'index']);
-Route::get('prodi',[ProdiController::class, 'index'] );
-Route::get('mahasiswa',[MahasiswaController::class, 'index']);
+// Route::get('fakultas', [FakultasController::class, 'index']);
+// Route::get('prodi',[ProdiController::class, 'index'] );
+// Route::get('mahasiswa',[MahasiswaController::class, 'index']);
 
-Route::patch('fakultas/{id}', [FakultasController::class, 'update']);
-Route::delete('fakultas/{id}', [FakultasController::class, 'destroy']);
+// Route::patch('fakultas/{id}', [FakultasController::class, 'update']);
+// Route::delete('fakultas/{id}', [FakultasController::class, 'destroy']);
 
-Route::middleware('auth:sanctum')->get('fakultas', [FakultasController::class, 'index']);
+// Route::middleware('auth:sanctum')->get('fakultas', [FakultasController::class, 'index']);
 Route::middleware(['auth:sanctum', 'ability:read-fakultas'])->get('fakultas', [FakultasController::class, 'index']);
 Route::middleware(['auth:sanctum', 'ability:create-fakultas'])->post('fakultas', [FakultasController::class, 'store']);
 Route::middleware(['auth:sanctum', 'ability:update-fakultas'])->patch('fakultas/{id}', [FakultasController::class, 'update']);
 Route::middleware(['auth:sanctum', 'ability:delete-fakultas'])->delete('fakultas/{id}', [FakultasController::class, 'destroy']);
 
-Route::post('fakultas', [FakultasController::class, 'store']);
-Route::post('prodi', [ProdiController::class, 'store']);
-Route::post('mahasiswa', [MahasiswaController::class, 'store']);
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+// Route::post('fakultas', [FakultasController::class, 'store']);
+// Route::post('prodi', [ProdiController::class, 'store']);
+// Route::post('mahasiswa', [MahasiswaController::class, 'store']);
+// Route::post('register', [AuthController::class, 'register']);
+// Route::post('login', [AuthController::class, 'login']);
 
